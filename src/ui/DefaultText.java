6b7850d5936c44552bcc383c81108151aaaa8a98
Daniel Knorreck <daniel.knorreck@telecom-paristech.fr>
2010-07-27 15:22:53 +0000

    update on build version: builder.txt

--- src/ui/DefaultText.java
+++ src/ui/DefaultText.java
@@ -48,8 +48,8 @@ package ui;
 
 
 public class DefaultText  {
-    public static String BUILD = "6067";
-	public static String DATE = "2010/07/27 17:21:53 CET";
+    public static String BUILD = "6068";
+	public static String DATE = "2010/07/27 17:22:50 CET";
 
     public static StringBuffer sbAbout = makeAbout();
     
