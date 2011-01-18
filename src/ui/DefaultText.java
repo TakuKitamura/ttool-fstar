14e0fb68a90c838edb1f0042fbf711de9914d230
Ludovic Apvrille <ludovic.apvrille@telecom-paristech.fr>
2011-01-18 09:08:55 +0000

    update on build version: builder.txt

--- src/ui/DefaultText.java
+++ src/ui/DefaultText.java
@@ -48,8 +48,8 @@ package ui;
 
 
 public class DefaultText  {
-    public static String BUILD = "6941";
-	public static String DATE = "2011/01/18 10:07:46 CET";
+    public static String BUILD = "6942";
+	public static String DATE = "2011/01/18 10:08:54 CET";
 
     public static StringBuffer sbAbout = makeAbout();
     
