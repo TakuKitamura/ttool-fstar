7cee17ca036916113eb9807727b3bc5dc1a2563a
Dominique Blouin <dominique.blouin@telecom-paristech.fr>
2011-02-13 20:01:12 +0000

    update on build version: builder.txt

--- src/ui/DefaultText.java
+++ src/ui/DefaultText.java
@@ -49,8 +49,8 @@ package ui;
 
 public class DefaultText  {
 
-    public static String BUILD = "7506";
-	public static String DATE = "2011/02/11 17:40:41 CET";
+    public static String BUILD = "7507";
+	public static String DATE = "2011/02/13 21:01:12 CET";
 
     public static StringBuffer sbAbout = makeAbout();
     
