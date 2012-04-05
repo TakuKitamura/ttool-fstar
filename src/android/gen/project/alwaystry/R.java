e52e53fcf53d9184cad42b9ba4d96806dd6e2657
Dominique Blouin <dominique.blouin@telecom-paristech.fr>
2012-04-05 12:34:47 +0000

    Modifications to the blocks

--- src/android/gen/project/alwaystry/R.java
+++ src/android/gen/project/alwaystry/R.java
@@ -11,18 +11,35 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int starting_logo=0x7f020001;
+        public static final int avatarbdcomp=0x7f020000;
+        public static final int avatarbdlink=0x7f020001;
+        public static final int avatarblock=0x7f020002;
+        public static final int avatardatatype=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int starting_logo=0x7f020005;
     }
     public static final class id {
         public static final int blockrectangle1=0x7f050002;
+        public static final int button1=0x7f05000c;
+        public static final int button2=0x7f05000d;
         public static final int drageventblock1=0x7f050001;
+        public static final int editText1=0x7f05000a;
+        public static final int imageButton1=0x7f050004;
+        public static final int imageButton2=0x7f050005;
+        public static final int imageButton3=0x7f050006;
+        public static final int imageButton4=0x7f050007;
+        public static final int imageView1=0x7f050008;
+        public static final int linearLayout1=0x7f050003;
         public static final int relativeLayout1=0x7f050000;
+        public static final int tabhost1=0x7f05000b;
+        public static final int textView1=0x7f050009;
     }
     public static final class layout {
         public static final int anothertab=0x7f030000;
         public static final int block=0x7f030001;
-        public static final int main=0x7f030002;
+        public static final int blocknamealert=0x7f030002;
+        public static final int editattributesalert=0x7f030003;
+        public static final int main=0x7f030004;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
