11e5ee3f6228c4652fbb311be44efdc437326531
Dominique Blouin <dominique.blouin@telecom-paristech.fr>
2012-06-27 10:00:20 +0000

    compositionconnector

--- src/android/src/project/alwaystry/AvatarBDCompositionConnectorAndroid.java
+++ src/android/src/project/alwaystry/AvatarBDCompositionConnectorAndroid.java
@@ -27,33 +27,12 @@ public class AvatarBDCompositionConnectorAndroid extends TGConnectorAndroid{
 		paint.setColor(Color.BLACK);
 	}
 	
-//	public boolean hasStart(){/*
-//		if(tx !=-1 && ty !=-1){
-//			return true;
-//		}else{
-//			return false;
-//		}	*/
-//		if(outPoint != null){
-//			return true;
-//		}
-//		return false;
-//	}
-//	
-//	public boolean hasEnd(){
-////		if(bx !=-1 && by !=-1){
-////			return true;
-////		}else{
-////			return false;
-////		}
-//		if(inPoint != null){
-//			return true;
-//		}
-//		return false;
-//	}
 	
 	public void internalDrawing(Canvas canvas){
 		
 		if(p1.isFree() || p2.isFree()){
+			p1.setState(TGConnectingPointAndroid.NORMAL);
+			p2.setState(TGConnectingPointAndroid.NORMAL);
 			((AvatarBDPanelAndroid)panel).getCompolist().remove(this);
 			return;
 		}
@@ -71,86 +50,68 @@ public class AvatarBDCompositionConnectorAndroid extends TGConnectorAndroid{
 		int dd = d;
 		int DD = D;
 		
-	//	if(hasStart())
+		int x1 =p1.getX();
+		int y1 =p1.getY();
+		int x2 =p2.getX();
+		int y2 = p2.getY();
 		
-	//	if(hasStart() && hasEnd()){
-			int x1 =p1.getX();
-			int y1 =p1.getY();
-			int x2 =p2.getX();
-			int y2 = p2.getY();
-			
-			canvas.drawLine(x1, y1, x2, y2, paint);
-			double xd[] = new double[4];
-			double yd[] = new double[4];
-			
-			if(x1 == x2){
-				if (y1 > y2) {
-					xd[0]=x2; yd[0]=y2+DD;
-					xd[1]=x2+(dd/2); yd[1]=y2+(DD/2);
-					xd[2]=x2; yd[2]=y2;
-					xd[3]=x2-(dd/2); yd[3]=y2+(DD/2);
-				}else{
-					xd[0]=x2; yd[0]=y2-DD;
-					xd[1]=x2+(dd/2); yd[1]=y2-(DD/2);
-					xd[2]=x2; yd[2]=y2;
-					xd[3]=x2-(dd/2); yd[3]=y2-(DD/2);
-				}
+		canvas.drawLine(x1, y1, x2, y2, paint);
+		double xd[] = new double[4];
+		double yd[] = new double[4];
+		
+		if(x1 == x2){
+			if (y1 > y2) {
+				xd[0]=x2; yd[0]=y2+DD;
+				xd[1]=x2+(dd/2); yd[1]=y2+(DD/2);
+				xd[2]=x2; yd[2]=y2;
+				xd[3]=x2-(dd/2); yd[3]=y2+(DD/2);
 			}else{
-				xd[0] = x2;
-				yd[0] = y2;
-				
-				int x0 = x1 - x2;
-				int y0 = y1 - y2;
-				double k = 1/(Math.sqrt((x0*x0)+(y0*y0)));
-				double u = x0*k;
-				double v = y0*k;
-				
-				double Ex = DD*u;
-				double Ey = DD*v;
-				double Fx = dd*v;
-				double Fy = -dd*u;
-				
-				//Q
-				xd[1] = x2+((Ex+Fx)/2);
-				yd[1] = y2+((Ey+Fy)/2);
-				
-				//R
-				xd[2] = x2+Ex;
-				yd[2] = y2+Ey;
-				
-				//S
-				xd[3] = xd[1] - Fx;
-				yd[3] = yd[1] - Fy;
+				xd[0]=x2; yd[0]=y2-DD;
+				xd[1]=x2+(dd/2); yd[1]=y2-(DD/2);
+				xd[2]=x2; yd[2]=y2;
+				xd[3]=x2-(dd/2); yd[3]=y2-(DD/2);
 			}
+		}else{
+			xd[0] = x2;
+			yd[0] = y2;
 			
-			Path path = new Path();
-			path.moveTo((float)xd[0], (float)yd[0]);
-			path.lineTo((float)xd[1], (float)yd[1]);
-			path.lineTo((float)xd[2], (float)yd[2]);
-			path.lineTo((float)xd[3], (float)yd[3]);
-			path.lineTo((float)xd[0], (float)yd[0]);
+			int x0 = x1 - x2;
+			int y0 = y1 - y2;
+			double k = 1/(Math.sqrt((x0*x0)+(y0*y0)));
+			double u = x0*k;
+			double v = y0*k;
 			
-			paint.setStyle(Paint.Style.FILL);
+			double Ex = DD*u;
+			double Ey = DD*v;
+			double Fx = dd*v;
+			double Fy = -dd*u;
 			
-			canvas.drawPath(path, paint);
-	//	}
+			//Q
+			xd[1] = x2+((Ex+Fx)/2);
+			yd[1] = y2+((Ey+Fy)/2);
+			
+			//R
+			xd[2] = x2+Ex;
+			yd[2] = y2+Ey;
+			
+			//S
+			xd[3] = xd[1] - Fx;
+			yd[3] = yd[1] - Fy;
+		}
+		
+		Path path = new Path();
+		path.moveTo((float)xd[0], (float)yd[0]);
+		path.lineTo((float)xd[1], (float)yd[1]);
+		path.lineTo((float)xd[2], (float)yd[2]);
+		path.lineTo((float)xd[3], (float)yd[3]);
+		path.lineTo((float)xd[0], (float)yd[0]);
+		
+		paint.setStyle(Paint.Style.FILL);
+		
+		canvas.drawPath(path, paint);
+
 	}
 
-//	public AvatarBDConnectingPointAndroid getOutPoint() {
-//		return outPoint;
-//	}
-//
-//	public void setOutPoint(AvatarBDConnectingPointAndroid outPoint) {
-//		this.outPoint = outPoint;
-//	}
-//
-//	public AvatarBDConnectingPointAndroid getInPoint() {
-//		return inPoint;
-//	}
-//
-//	public void setInPoint(AvatarBDConnectingPointAndroid inPoint) {
-//		this.inPoint = inPoint;
-//	}
 
 	public LinkedList getInternalpoints() {
 		return internalpoints;
@@ -165,7 +126,6 @@ public class AvatarBDCompositionConnectorAndroid extends TGConnectorAndroid{
 	}
 
 	protected boolean editOndoubleClick(int _x, int _y) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 }
