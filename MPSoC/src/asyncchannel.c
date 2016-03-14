ad4b4e229397a468f37367e7c98d83d7ff031c24
Daniela Genius <Daniela.Genius@lip6.fr>
2016-03-14 13:52:06 +0000

    cleaned debug messages

--- MPSoC/src/asyncchannel.c
+++ MPSoC/src/asyncchannel.c
@@ -3,12 +3,8 @@
 #include "message.h"
 #include "asyncchannel.h"
 #include "myerrors.h"
-// ajoute DG
+
 #include "mwmr.h"
-//fin ajoute DG
-
-
-//ajoute DG
 
 /* this function tries to read one message from the channel and returns either 
  1 or 0*/
@@ -26,15 +22,14 @@ int async_write_nonblocking( struct mwmr_s *fifo, void *_ptr, int lensw ){
   i = mwmr_try_write(fifo,_ptr,lensw);
   if (i<lensw){
     /* the data item is thrown away */
-    //printf("data thrown away");
+    //debugInt("data thrown away");
     return i;
   }
   else{
-    //printf("data transmitted");
+    //debugInt("data transmitted");
   }
   return i;
 }
-
 
 
 void async_read( struct mwmr_s *fifo, void *_ptr, int lensw ){
@@ -45,9 +40,6 @@ void async_write( struct mwmr_s *fifo, void *_ptr, int lensw ){
   mwmr_write(fifo,_ptr,lensw);
   }
 
-//fin ajoute DG
-
-//DG 7.9. add MWMR as parameter
 asyncchannel *getNewAsyncchannel(char *outname, char *inname, int isBlocking, int maxNbOfMessages, struct mwmr_s *fifo) {
   asyncchannel * asyncch = (asyncchannel *)(malloc(sizeof(struct asyncchannel)));
   if (asyncch == NULL) {
@@ -57,14 +49,13 @@ asyncchannel *getNewAsyncchannel(char *outname, char *inname, int isBlocking, in
   asyncch->outname = outname;
   asyncch->isBlocking = isBlocking;
   asyncch->maxNbOfMessages = maxNbOfMessages;
-  //DG 7.9. add MWMR
-  // DG 08.12. ici bug : voir CAVE
+  
   asyncch->mwmr_fifo=fifo;
   asyncch->mwmr_fifo->depth=fifo->depth;
   asyncch->mwmr_fifo->width=fifo->width;
-printf("asyncchannel getNew %x \n",asyncch->mwmr_fifo);
-printf("asyncchannel %x \n",asyncch->mwmr_fifo->depth);
-printf("asyncchannel %x \n",asyncch->mwmr_fifo->width);
+  debugInt("asyncchannel getNew %x \n",asyncch->mwmr_fifo);
+  debugInt("asyncchannel %x \n",asyncch->mwmr_fifo->depth);
+  debugInt("asyncchannel %x \n",asyncch->mwmr_fifo->width);
 
   return asyncch;
 }
@@ -72,8 +63,6 @@ printf("asyncchannel %x \n",asyncch->mwmr_fifo->width);
 void destroyAsyncchannel(asyncchannel *asyncch) {
   free(asyncch);
 }
-
-/* DG il faut en meme temps gerer le manager central et les canaux MWMR */
 
   message* getAndRemoveOldestMessageFromAsyncChannel(asyncchannel *channel) {
   message *msg;
@@ -85,12 +74,13 @@ void destroyAsyncchannel(asyncchannel *asyncch) {
 
   if (channel->currentNbOfMessages == 1) {
     channel->currentNbOfMessages = 0;
-    msg = channel->pendingMessages;//DG 08.12. p.e. plus besoin de cela?
+    msg = channel->pendingMessages;
     channel->pendingMessages = NULL;
-   
-  printf("£££££££££££££££££££££££££££\n");  printf("before async read 0\n");printf("£££££££££££££££££££££££££££\n");
+  
+    debugInt("asyncchannel read: address %x \n",channel->mwmr_fifo);
+    debugInt("asyncchannel %x \n",channel->mwmr_fifo->depth);
+    debugInt("asyncchannel %x \n",channel->mwmr_fifo->width);
     async_read(channel->mwmr_fifo, &msg, 1);
- printf("£££££££££££££££££££££££££££\n");printf("after async read 0\n");printf("£££££££££££££££££££££££££££\n");
     return msg;
   }
 
@@ -103,26 +93,24 @@ void destroyAsyncchannel(asyncchannel *asyncch) {
 
   channel->currentNbOfMessages = channel->currentNbOfMessages -1;
   previous->next = NULL;
-  //async_read(channel->mwmr_fifo, msg, 1);
-printf("£££££££££££££££££££££££££££\n");printf("before async read 1");printf("£££££££££££££££££££££££££££\n");
-  async_read(channel->mwmr_fifo, &msg, 1);//DG 08.12.
-printf("£££££££££££££££££££££££££££\n");printf("after async read 1\n");printf("£££££££££££££££££££££££££££\n");
+  debugInt("before async read 1\n");
+  debugInt("asyncchannel address %x \n",channel->mwmr_fifo);
+  debugInt("asyncchannel %x \n",channel->mwmr_fifo->depth);
+  debugInt("asyncchannel %x \n",channel->mwmr_fifo->width);
+  async_read(channel->mwmr_fifo, &msg, 1);
+  
   return msg;
 }
 
 void addMessageToAsyncChannel(asyncchannel *channel, message *msg) {
  
-  /* DG on ajoute un champ a la structure channel */ 
   msg->next = channel->pendingMessages;
   channel->pendingMessages = msg;
   channel->currentNbOfMessages = channel->currentNbOfMessages+1;
-printf("asyncchannel address %x \n",channel->mwmr_fifo);
-printf("asyncchannel %x \n",channel->mwmr_fifo->depth);
-printf("asyncchannel %x \n",channel->mwmr_fifo->width);
+  debugInt("asyncchannel write: address %x \n",channel->mwmr_fifo);
+  debugInt("asyncchannel %x \n",channel->mwmr_fifo->depth);
+  debugInt("asyncchannel %x \n",channel->mwmr_fifo->width); 
+  debugInt("channel->mwmr_fifo: %x \n",channel->mwmr_fifo);
+  async_write(channel->mwmr_fifo, &msg, 1 );
 
-printf("£££££££££££££££££££££££££££\n");printf("before async write 0");printf("£££££££££££££££££££££££££££\n");
-printf("channel->mwmr_fifo: %x \n",channel->mwmr_fifo);
-async_write(channel->mwmr_fifo, &msg, 1 );
- printf("£££££££££££££££££££££££££££\n");
-printf("after async write 0\n");printf("£££££££££££££££££££££££££££\n");
 }
