458c1df3112c29aef59455e3457d0b7251bf44b9
Letitia Li <leli@enst.fr>
2016-03-24 16:29:57 +0000

    DIPLODOCUS Encrypt and Decrypt

---
+++ src/tmltranslator/SecurityPattern.java
@@ -0,0 +1,13 @@
+package tmltranslator;
+
+public class SecurityPattern {
+
+public String name;
+public int keySize;
+public int MAC=0;
+
+public SecurityPattern(String _name, String _keySize, String _MACSize){
+
+}
+
+}
