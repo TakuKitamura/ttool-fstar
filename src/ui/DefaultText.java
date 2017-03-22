ffd3afd09e6106d9ebbaf19296e0999ef8263394
apvrille <ludovic.apvrille@eurecom.fr>
2017-03-22 02:01:04 +0100

    update on build version: builder.txt

--- src/ui/DefaultText.java
+++ src/ui/DefaultText.java
@@ -49,8 +49,8 @@ package ui;
 
 public class DefaultText  {
 
-    public static String BUILD = "12166";
-    public static String DATE = "2017/03/21 02:01:02 CET";
+    public static String BUILD = "12167";
+    public static String DATE = "2017/03/22 02:01:03 CET";
     
     
     public static StringBuffer sbAbout = makeAbout();
