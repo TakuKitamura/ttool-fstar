15c307d4411d79a9de9487e2747bc3f5fcd5c8be
Ludovic Apvrille <ludovic.apvrille@telecom-paristech.fr>
2017-06-15 16:02:11 +0200

    Missing separator in TTool

--- tests/util/fr.tpt.ttool.tests.util/src/fr/tpt/ttool/tests/util/remote/TestRshClient.java
+++ tests/util/fr.tpt.ttool.tests.util/src/fr/tpt/ttool/tests/util/remote/TestRshClient.java
@@ -21,311 +21,311 @@ import myutil.FileException;
 import myutil.FileUtils;
 
 public class TestRshClient {
-	
-	private static final String EXPECTED_COMMAND_OUTPUT = "!!!Hello World!!!" + System.lineSeparator();
-	private static final String TEST_PROGRAM_NAME = "helloWorld";
-	private static final String TEST_COMMAND = "./resources/" + TEST_PROGRAM_NAME;
-	private static final String TEST_COMMAND_NON_STOP = "./resources/helloWorldNonStop";
-	private static final String TEST_FILE_NAME = "./resources/test.txt";
-	private static final String TEST_FILE_DATA = "testDatafhkenomrcg ,jgh o";
 
-	
-	private RshClient client = null;
-	private static Thread serverThread = null;
+    private static final String EXPECTED_COMMAND_OUTPUT = "!!!Hello World!!!" + System.lineSeparator();
+    private static final String TEST_PROGRAM_NAME = "helloWorld";
+    private static final String TEST_COMMAND = "./resources/" + TEST_PROGRAM_NAME;
+    private static final String TEST_COMMAND_NON_STOP = "./resources/helloWorldNonStop";
+    private static final String TEST_FILE_NAME = "./resources/test.txt";
+    private static final String TEST_FILE_DATA = "testDatafhkenomrcg ,jgh o";
 
-	@BeforeClass
-	public static void setUpBeforeClass()
-	throws Exception {
+
+    private RshClient client = null;
+    private static Thread serverThread = null;
+
+    @BeforeClass
+    public static void setUpBeforeClass()
+        throws Exception {
         RshClient.PORT_NUMBER = 8080;
         RshServer.PORT_NUMBER = RshClient.PORT_NUMBER;
 
         final Runnable runnable = new Runnable() {
-			
-			@Override
-			public void run() {
-				new RshServer( null ).startServer();
-			}
-		};
-		
-		serverThread = new Thread( runnable );
-		serverThread.start();
-		Thread.sleep( 500 );
-	}
 
-	@Before
-	public void setUp()
-	throws Exception {
-		client = new RshClient( "localhost" );
-	}
+                @Override
+                public void run() {
+                    new RshServer( null ).startServer();
+                }
+            };
 
-	@After
-	public void tearDown()
-	throws Exception {
-		client = null;
-	}
+        serverThread = new Thread( runnable );
+        serverThread.start();
+        Thread.sleep( 500 );
+    }
 
-	@AfterClass
-	public static void tearDownAfterClass()
-	throws Exception {
-		serverThread.interrupt();
-	}
-	
-	private void handleException( final Throwable th ) {
-		th.printStackTrace();
-		fail( th.getLocalizedMessage() );
-	}
+    @Before
+    public void setUp()
+        throws Exception {
+        client = new RshClient( "localhost" );
+    }
 
-	@Test
-	public void testStopCommand() {
+    @After
+    public void tearDown()
+        throws Exception {
+        client = null;
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass()
+        throws Exception {
+        serverThread.interrupt();
+    }
+
+    private void handleException( final Throwable th ) {
+        th.printStackTrace();
+        fail( th.getLocalizedMessage() );
+    }
+
+    @Test
+    public void testStopCommand() {
         final Runnable runnable = new Runnable() {
-			
-			@Override
-			public void run() {
-				try {
-					client.setCmd( TEST_COMMAND_NON_STOP );
-					client.sendExecuteCommandRequest();
-					final Writer writer = new StringWriter();
-					client.writeCommandMessages( writer );
-				}
-				catch (LauncherException e) {
-					handleException( e );
-				}
-			}
-		};
-		
-		final Thread thread = new Thread( runnable );
-		thread.start();
-		
-		try {
-			
-			// Ensure the remote process has enough time to start
-			Thread.sleep( 200 );
-			client.stopCommand();
 
-			// Ensure the stop command has been processed on the server
-			Thread.sleep( 200 );
-			assertTrue( killUnexistentProcess() );
-		}
-		catch ( LauncherException ex ) {
-			handleException( ex );
-		}
-		catch ( InterruptedException ex ) {
-			handleException( ex );
-		}
-	}
+                @Override
+                public void run() {
+                    try {
+                        client.setCmd( TEST_COMMAND_NON_STOP );
+                        client.sendExecuteCommandRequest();
+                        final Writer writer = new StringWriter();
+                        client.writeCommandMessages( writer );
+                    }
+                    catch (LauncherException e) {
+                        handleException( e );
+                    }
+                }
+            };
 
-	private boolean killUnexistentProcess() {
-		try {
-			client.sendKillProcessRequest();
-			
-			return false;
-		}
-		catch ( LauncherException ex ) {
-			return RshClient.FAILED.equals( ex.getMessage() );
-		}
-	}
+        final Thread thread = new Thread( runnable );
+        thread.start();
 
-	@Test
-	public void testGetId() {
-		try {
-			final int id = client.getId();
-			assertTrue( id == 1 );
-		}
-		catch ( LauncherException ex ) {
-			handleException( ex );
-		}
-	}
+        try {
 
-	@Test
-	public void testFreeId() {
-		try {
-			client.freeId( 1 );
-		}
-		catch ( LauncherException ex ) {
-			handleException( ex );
-		}
-	}
+            // Ensure the remote process has enough time to start
+            Thread.sleep( 200 );
+            client.stopCommand();
 
-	@Test
-	public void testSendExecuteCommandRequest() {
-		client.setCmd( TEST_COMMAND );
-		
-		try {
-			client.sendExecuteCommandRequest();
-			final Writer writer = new StringWriter();
-			client.writeCommandMessages( writer );
-			assertTrue( ( EXPECTED_COMMAND_OUTPUT + System.lineSeparator() ).equals( writer.toString() ) );
-		}
-		catch ( LauncherException ex ) {
-			handleException( ex );
-		}
-	}
+            // Ensure the stop command has been processed on the server
+            Thread.sleep( 200 );
+            assertTrue( killUnexistentProcess() );
+        }
+        catch ( LauncherException ex ) {
+            handleException( ex );
+        }
+        catch ( InterruptedException ex ) {
+            handleException( ex );
+        }
+    }
 
-	@Test
-	public void testSendExecuteCommandRequestBoolean() {
-		client.setCmd( TEST_COMMAND );
-		
-		try {
-			client.sendExecuteCommandRequest( true );
-			final Writer writer = new StringWriter();
-			client.writeCommandMessages( writer );
-			assertTrue( writer.toString().startsWith( EXPECTED_COMMAND_OUTPUT ) );
+    private boolean killUnexistentProcess() {
+        try {
+            client.sendKillProcessRequest();
 
-			final Integer retCode = client.getProcessReturnCode();
-			assertTrue( retCode != null && retCode == 0  );
-		}
-		catch ( LauncherException ex ) {
-			handleException( ex );
-		}
-	}
+            return false;
+        }
+        catch ( LauncherException ex ) {
+            return RshClient.FAILED.equals( ex.getMessage() );
+        }
+    }
 
-	@Test
-	public void testSendExecutePipedCommandsRequest() {
-		final String testFileName = "./resources/test_piped_commands.txt";
-		final String expectedData = "Test Passed!" + System.lineSeparator();
-		
-		try {
-			FileUtils.saveFile( testFileName, expectedData );
-			client.sendExecutePipedCommandsRequest( "echo " + testFileName, "xargs cat" );
-			final String data = client.getDataFromProcess();
-			
-			assertTrue( "Piped commands returned " + data, expectedData.equals( data ) );
-		}
-		catch ( LauncherException ex ) {
-			handleException( ex );
-		}
-		catch ( FileException ex ) {
-			handleException( ex );
-		}
-		finally {
-			new File( testFileName ).delete();
-		}
-	}
+    @Test
+    public void testGetId() {
+        try {
+            final int id = client.getId();
+            assertTrue( id == 1 );
+        }
+        catch ( LauncherException ex ) {
+            handleException( ex );
+        }
+    }
 
-	private boolean deleteTestFile() {
-		final File testFile = new File( TEST_FILE_NAME );
-		
-		if ( testFile.exists() ) {
-			assertTrue(  "Test file could not be deleted!", testFile.delete() );
-		}
-		
-		return true;
-	}
+    @Test
+    public void testFreeId() {
+        try {
+            client.freeId( 1 );
+        }
+        catch ( LauncherException ex ) {
+            handleException( ex );
+        }
+    }
 
-	@Test
-	public void testSendFileData() {
-		deleteTestFile();
-		
-		try {
-			client.sendFileData( TEST_FILE_NAME, TEST_FILE_DATA );
-			
-			try {
-				final String readData = FileUtils.loadFile( TEST_FILE_NAME );
-				
-				assertTrue( ( TEST_FILE_DATA + System.lineSeparator() ).equals( readData ) );
-			}
-			catch ( FileException ex ) {
-				handleException( ex );
-			}
-		}
-		catch( LauncherException ex ) {
-			handleException( ex );
-		}
-	}
+    @Test
+    public void testSendExecuteCommandRequest() {
+        client.setCmd( TEST_COMMAND );
 
-	@Test
-	public void testGetFileData() {
-		deleteTestFile();
+        try {
+            client.sendExecuteCommandRequest();
+            final Writer writer = new StringWriter();
+            client.writeCommandMessages( writer );
+            assertTrue( ( EXPECTED_COMMAND_OUTPUT + System.lineSeparator() ).equals( writer.toString() ) );
+        }
+        catch ( LauncherException ex ) {
+            handleException( ex );
+        }
+    }
 
-		try {
-			FileUtils.saveFile( TEST_FILE_NAME, TEST_FILE_DATA );
+    @Test
+    public void testSendExecuteCommandRequestBoolean() {
+        client.setCmd( TEST_COMMAND );
 
-			final String readData = client.getFileData( TEST_FILE_NAME );
-			
-			assertTrue( TEST_FILE_DATA.equals( readData ) );
-		} 
-		catch ( FileException ex ) {
-			handleException( ex );
-		}
-		catch ( LauncherException ex ) {
-			handleException( ex );
-		}
-	}
+        try {
+            client.sendExecuteCommandRequest( true );
+            final Writer writer = new StringWriter();
+            client.writeCommandMessages( writer );
+            assertTrue( writer.toString().startsWith( EXPECTED_COMMAND_OUTPUT ) );
 
-	@Test
-	public void testDeleteFile() {
-		deleteTestFile();
+            final Integer retCode = client.getProcessReturnCode();
+            assertTrue( retCode != null && retCode == 0  );
+        }
+        catch ( LauncherException ex ) {
+            handleException( ex );
+        }
+    }
 
-		try {
-			FileUtils.saveFile( TEST_FILE_NAME, TEST_FILE_DATA );
+    @Test
+    public void testSendExecutePipedCommandsRequest() {
+        final String testFileName = "./resources/test_piped_commands.txt";
+        final String expectedData = "Test Passed!" + System.lineSeparator();
 
-			client.deleteFile( TEST_FILE_NAME );
-			
-			assertFalse( new File( TEST_FILE_NAME ).exists() );
-		} 
-		catch ( FileException ex ) {
-			handleException( ex );
-		}
-		catch ( LauncherException ex ) {
-			handleException( ex );
-		}
-	}
+        try {
+            FileUtils.saveFile( testFileName, expectedData );
+            client.sendExecutePipedCommandsRequest( "echo " + testFileName, "xargs cat" );
+            final String data = client.getDataFromProcess();
 
-	@Test
-	public void testSendKillProcessRequest() {
-		client.setCmd( TEST_COMMAND_NON_STOP );
-		
-		try {
-			client.sendExecuteCommandRequest();
-			Thread.sleep( 200 );
-			client.sendKillProcessRequest();
+            assertTrue( "Piped commands returned " + data, expectedData.equals( data ) );
+        }
+        catch ( LauncherException ex ) {
+            handleException( ex );
+        }
+        catch ( FileException ex ) {
+            handleException( ex );
+        }
+        finally {
+            new File( testFileName ).delete();
+        }
+    }
 
-			Thread.sleep( 200 );
-			assertTrue( killUnexistentProcess() );
-		}
-		catch ( LauncherException ex ) {
-			handleException( ex );
-		}
-		catch ( InterruptedException ex ) {
-			handleException( ex );
-		}
-	}
+    private boolean deleteTestFile() {
+        final File testFile = new File( TEST_FILE_NAME );
 
-	@Test
-	public void testSendKillAllProcessRequest() {
-		client.setCmd( TEST_COMMAND_NON_STOP );
-		
-		try {
-			for ( int index = 0; index < 4; index++ ) {
-				client.sendExecuteCommandRequest();
-				Thread.sleep( 200 );
-			}
-			
-			client.sendKillAllProcessRequest();
+        if ( testFile.exists() ) {
+            assertTrue(  "Test file could not be deleted!", testFile.delete() );
+        }
 
-			Thread.sleep( 200 );
-			assertTrue( killUnexistentProcess() );
-		}
-		catch ( LauncherException ex ) {
-			handleException( ex );
-		}
-		catch ( InterruptedException ex ) {
-			handleException( ex );
-		}
-	}
+        return true;
+    }
 
-	@Test
-	public void testGetDataFromProcess() {
-		client.setCmd( TEST_COMMAND );
-		
-		try {
-			client.sendExecuteCommandRequest();
-			final String messageFromProcess = client.getDataFromProcess();
-			
-			assertTrue( ( EXPECTED_COMMAND_OUTPUT ).equals( messageFromProcess ) );
-		}
-		catch ( LauncherException ex ) {
-			handleException( ex );
-		}
-	}
+    @Test
+    public void testSendFileData() {
+        deleteTestFile();
+
+        try {
+            client.sendFileData( TEST_FILE_NAME, TEST_FILE_DATA );
+
+            try {
+                final String readData = FileUtils.loadFile( TEST_FILE_NAME );
+
+                assertTrue( ( TEST_FILE_DATA + System.lineSeparator() ).equals( readData ) );
+            }
+            catch ( FileException ex ) {
+                handleException( ex );
+            }
+        }
+        catch( LauncherException ex ) {
+            handleException( ex );
+        }
+    }
+
+    @Test
+    public void testGetFileData() {
+        deleteTestFile();
+
+        try {
+            FileUtils.saveFile( TEST_FILE_NAME, TEST_FILE_DATA );
+
+            final String readData = client.getFileData( TEST_FILE_NAME );
+
+            assertTrue( TEST_FILE_DATA.equals( readData ) );
+        }
+        catch ( FileException ex ) {
+            handleException( ex );
+        }
+        catch ( LauncherException ex ) {
+            handleException( ex );
+        }
+    }
+
+    @Test
+    public void testDeleteFile() {
+        deleteTestFile();
+
+        try {
+            FileUtils.saveFile( TEST_FILE_NAME, TEST_FILE_DATA );
+
+            client.deleteFile( TEST_FILE_NAME );
+
+            assertFalse( new File( TEST_FILE_NAME ).exists() );
+        }
+        catch ( FileException ex ) {
+            handleException( ex );
+        }
+        catch ( LauncherException ex ) {
+            handleException( ex );
+        }
+    }
+
+    @Test
+    public void testSendKillProcessRequest() {
+        client.setCmd( TEST_COMMAND_NON_STOP );
+
+        try {
+            client.sendExecuteCommandRequest();
+            Thread.sleep( 200 );
+            client.sendKillProcessRequest();
+
+            Thread.sleep( 200 );
+            assertTrue( killUnexistentProcess() );
+        }
+        catch ( LauncherException ex ) {
+            handleException( ex );
+        }
+        catch ( InterruptedException ex ) {
+            handleException( ex );
+        }
+    }
+
+    @Test
+    public void testSendKillAllProcessRequest() {
+        client.setCmd( TEST_COMMAND_NON_STOP );
+
+        try {
+            for ( int index = 0; index < 4; index++ ) {
+                client.sendExecuteCommandRequest();
+                Thread.sleep( 200 );
+            }
+
+            client.sendKillAllProcessRequest();
+
+            Thread.sleep( 200 );
+            assertTrue( killUnexistentProcess() );
+        }
+        catch ( LauncherException ex ) {
+            handleException( ex );
+        }
+        catch ( InterruptedException ex ) {
+            handleException( ex );
+        }
+    }
+
+    @Test
+    public void testGetDataFromProcess() {
+        client.setCmd( TEST_COMMAND );
+
+        try {
+            client.sendExecuteCommandRequest();
+            final String messageFromProcess = client.getDataFromProcess();
+
+            assertTrue( ( EXPECTED_COMMAND_OUTPUT ).equals( messageFromProcess ) );
+        }
+        catch ( LauncherException ex ) {
+            handleException( ex );
+        }
+    }
 }
