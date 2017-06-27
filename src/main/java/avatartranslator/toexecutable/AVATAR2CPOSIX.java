7ae4771f363f70f8634a20c99710c32daa87a0c5
apvrille <ludovic.apvrille@eurecom.fr>
2017-06-27 14:09:11 +0200

    Preparing plugin for TGComponents

--- src/main/java/avatartranslator/toexecutable/AVATAR2CPOSIX.java
+++ src/main/java/avatartranslator/toexecutable/AVATAR2CPOSIX.java
@@ -1,26 +1,26 @@
 /* Copyright or (C) or Copr. GET / ENST, Telecom-Paris, Ludovic Apvrille
- * 
+ *
  * ludovic.apvrille AT enst.fr
- * 
+ *
  * This software is a computer program whose purpose is to allow the
  * edition of TURTLE analysis, design and deployment diagrams, to
  * allow the generation of RT-LOTOS or Java code from this diagram,
  * and at last to allow the analysis of formal validation traces
  * obtained from external tools, e.g. RTL from LAAS-CNRS and CADP
  * from INRIA Rhone-Alpes.
- * 
+ *
  * This software is governed by the CeCILL  license under French law and
  * abiding by the rules of distribution of free software.  You can  use,
  * modify and/ or redistribute the software under the terms of the CeCILL
  * license as circulated by CEA, CNRS and INRIA at the following URL
  * "http://www.cecill.info".
- * 
+ *
  * As a counterpart to the access to the source code and  rights to copy,
  * modify and redistribute granted by the license, users are provided only
  * with a limited warranty  and the software's author,  the holder of the
  * economic rights,  and the successive licensors  have only  limited
  * liability.
- * 
+ *
  * In this respect, the user's attention is drawn to the risks associated
  * with loading,  using,  modifying and/or developing or reproducing the
  * software by the user in light of its specific status of free software,
@@ -31,7 +31,7 @@
  * requirements in conditions enabling the security of their systems and/or
  * data to be ensured and,  more generally, to use and operate it in the
  * same conditions as regards security.
- * 
+ *
  * The fact that you are presently reading this means that you have had
  * knowledge of the CeCILL license and that you accept its terms.
  */
@@ -88,7 +88,7 @@ public class AVATAR2CPOSIX {
 
     public AVATAR2CPOSIX(AvatarSpecification _avspec, String _plugin) {
         avspec = _avspec;
-	plugin = _plugin;
+        plugin = _plugin;
     }
 
     public void setTimeUnit(int _timeUnit) {
@@ -376,7 +376,7 @@ public class AVATAR2CPOSIX {
                     cpt ++;
                 }
                 ret+= ");" + CR;
-		//TraceManager.addDev("Adding a call to the method");
+                //TraceManager.addDev("Adding a call to the method");
 
             } else {
 
@@ -870,7 +870,7 @@ public class AVATAR2CPOSIX {
         g = Conversion.replaceOp(g, "and", "&&");
         g = Conversion.replaceOp(g, "or", "||");
         g = Conversion.replaceOp(g, "not", "!");
-	TraceManager.addDev("Guard=" + g);
+        TraceManager.addDev("Guard=" + g);
         return g;
     }
 
@@ -973,15 +973,15 @@ public class AVATAR2CPOSIX {
         String type;
         for(int i=0; i<_at.getNbOfAction(); i++) {
             // Must know whether this is an action or a method call
-	    
+
             AvatarAction act = _at.getAction(i);
-	    TraceManager.addDev("Action=" + act);
+            TraceManager.addDev("Action=" + act);
             if (act.isAMethodCall()) {
-		TraceManager.addDev("Method call");
+                TraceManager.addDev("Method call");
                 String actModified = modifyMethodName (_block, (AvatarTermFunction) act);
                 ret +=  actModified + ";" + CR;
             } else {
-		TraceManager.addDev("Else");
+                TraceManager.addDev("Else");
                 String actModified = modifyMethodName (_block, ((AvatarActionAssignment) act).getLeftHand ())
                     + " = " + modifyMethodName (_block, ((AvatarActionAssignment) act).getRightHand ());
                 AvatarLeftHand leftHand = ((AvatarActionAssignment) act).getLeftHand ();
