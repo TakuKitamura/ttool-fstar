0e9b2be1508b42243277e0ac14bd8754744f1a35
Fabien Tessier <fabien.tessier@telecom-paristech.fr>
2017-09-14 17:29:16 +0200

    Issue #68: Review Filters of Diagram Elements

--- src/main/java/ui/tmldd/TMLArchiDiagramToolBar.java
+++ src/main/java/ui/tmldd/TMLArchiDiagramToolBar.java
@@ -37,9 +37,6 @@
  * knowledge of the CeCILL license and that you accept its terms.
  */
 
-
-
-
 package ui.tmldd;
 
 import myutil.TraceManager;
@@ -48,6 +45,9 @@ import ui.TGUIAction;
 import ui.TToolBar;
 
 import javax.swing.*;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -55,14 +55,16 @@ import java.awt.event.ActionListener;
    * Class TMLArchiDiagramToolBar
    * Implements the toolbar to be used in conjunction with the panel of a TML Architecture diagram
    * Creation: 18/09/2007
-   * @version 1.0 08/09/2017
+   * @version 1.0 18/09/2007
    * @author Ludovic APVRILLE
    * @see ui.cd.TClassDiagramPanel
  */
 public class TMLArchiDiagramToolBar extends TToolBar  implements ActionListener {
 
-    protected JComboBox<String> box;
-    protected static String[] viewInfos = { "View all", "View architecture only", "View task mapping", "View channel mapping", "View Comm. Pattern", "View Comm. Pattern mapping", "View security mapping"};
+    protected JMenuBar box;
+    protected JMenu menu;
+    protected JCheckBoxMenuItem m1, m2, m3, m4, m5;
+    protected static String[] viewInfos = {"View task mapping", "View channel mapping", "View Comm. Pattern mapping", "View Port Interface", "View security mapping"};
     protected TMLArchiDiagramPanel panel;
 
     public TMLArchiDiagramToolBar(MainGUI _mgui) {
@@ -76,7 +78,6 @@ public class TMLArchiDiagramToolBar extends TToolBar  implements ActionListener 
         mgui.actions[TGUIAction.TMLARCHI_LINK].setEnabled(b);
         mgui.actions[TGUIAction.TMLARCHI_CPUNODE].setEnabled(b);
         mgui.actions[TGUIAction.TMLARCHI_HWANODE].setEnabled(b);
-	mgui.actions[TGUIAction.TMLARCHI_CAMSNODE].setEnabled(b); //ajout CD
         mgui.actions[TGUIAction.TMLARCHI_BUSNODE].setEnabled(b);
         mgui.actions[TGUIAction.TMLARCHI_CPNODE].setEnabled(b);
         mgui.actions[TGUIAction.TMLARCHI_BRIDGENODE].setEnabled(b);
@@ -125,8 +126,6 @@ public class TMLArchiDiagramToolBar extends TToolBar  implements ActionListener 
         button.addMouseListener(mgui.mouseHandler);
         button = this.add(mgui.actions[TGUIAction.TMLARCHI_HWANODE]);
         button.addMouseListener(mgui.mouseHandler);
-        button = this.add(mgui.actions[TGUIAction.TMLARCHI_CAMSNODE]); //ajout CD
-        button.addMouseListener(mgui.mouseHandler);
         button = this.add(mgui.actions[TGUIAction.TMLARCHI_ARTIFACT]);
         button.addMouseListener(mgui.mouseHandler);
 
@@ -143,26 +142,27 @@ public class TMLArchiDiagramToolBar extends TToolBar  implements ActionListener 
         button.addMouseListener(mgui.mouseHandler);
         button = this.add(mgui.actions[TGUIAction.TMLARCHI_MEMORYNODE]);
         button.addMouseListener(mgui.mouseHandler);
-	this.addSeparator();
+        this.addSeparator();
 
-	button = this.add(mgui.actions[TGUIAction.TMLARCHI_COMMUNICATION_ARTIFACT]);
+		button = this.add(mgui.actions[TGUIAction.TMLARCHI_COMMUNICATION_ARTIFACT]);
         button.addMouseListener(mgui.mouseHandler);
 	
 	/*button = this.add(mgui.actions[TGUIAction.TMLARCHI_EVENT_ARTIFACT]);
 	  button.addMouseListener(mgui.mouseHandler);*/
 	    
         if (MainGUI.experimentalOn) {
-	    this.addSeparator();
+        	this.addSeparator();
             button = this.add(mgui.actions[TGUIAction.TMLARCHI_CPNODE]);
             button.addMouseListener(mgui.mouseHandler);
-	    button = this.add(mgui.actions[TGUIAction.TMLARCHI_PORT_ARTIFACT]);
-	    button.addMouseListener(mgui.mouseHandler);
+            button = this.add(mgui.actions[TGUIAction.TMLARCHI_PORT_ARTIFACT]);
+            button.addMouseListener(mgui.mouseHandler);
         }
-	this.addSeparator();
+
+        this.addSeparator();
 	
         button = this.add(mgui.actions[TGUIAction.TMLARCHI_KEY]);
         button.addMouseListener(mgui.mouseHandler);
-	button = this.add(mgui.actions[TGUIAction.TMLARCHI_FIREWALL]);
+        button = this.add(mgui.actions[TGUIAction.TMLARCHI_FIREWALL]);
         button.addMouseListener(mgui.mouseHandler);
 
 	
@@ -175,31 +175,69 @@ public class TMLArchiDiagramToolBar extends TToolBar  implements ActionListener 
         button = this.add(mgui.actions[TGUIAction.ACT_TOGGLE_DIPLO_ID]);
         button.addMouseListener(mgui.mouseHandler);
 
-	setPluginButtons("TMLArchiDiagramPanel");
+        setPluginButtons("TMLArchiDiagramPanel");
 	
         this.addSeparator();
         if (viewInfos == null) {
             TraceManager.addDev("null viewInfos");
         }
-        box = new JComboBox<>(viewInfos);
-        this.add(box);
-        box.addActionListener(this);
+        
+        //Issue #68: Review Filters of Diagram Elements
+        box = new JMenuBar();
+        menu = new JMenu("Edit configuration view");
+        
+       	m1 = new JCheckBoxMenuItem(viewInfos[0], true);
+       	m1.addActionListener(this);
+       	m2 = new JCheckBoxMenuItem(viewInfos[1], true);
+       	m2.addActionListener(this);
+       	m3 = new JCheckBoxMenuItem(viewInfos[2], true);
+       	m3.addActionListener(this);
+       	m4 = new JCheckBoxMenuItem(viewInfos[3], true);
+       	m4.addActionListener(this);
+       	m5 = new JCheckBoxMenuItem(viewInfos[4], true);
+       	m5.addActionListener(this);
+       	
+       	
+        menu.add(m1);
+        menu.add(m2); 
+        menu.add(m3); 
+        menu.add(m4); 
+        menu.add(m5);
 
-	
+        box.add(menu);    
+        
+        this.add(box);
     }
 
     public void setPanel(TMLArchiDiagramPanel _panel) {
         panel = _panel;
     }
 
-
+    @Override
     public void actionPerformed(ActionEvent e) {
-	super.actionPerformed(e);
-        if (e.getSource() == box) {
+    	super.actionPerformed(e);
+        
+    	//if (e.getSource() == box) {
             if (panel != null) {
-                panel.setCurrentView(box.getSelectedIndex());
+            	int res = 0;
+            	
+            	//Check if "View Comm. Pattern mapping" is check to enable "Port Interface" option
+            	if (!((JCheckBoxMenuItem) box.getMenu(0).getItem(2)).getState())
+            		((JCheckBoxMenuItem)box.getMenu(0).getItem(3)).setEnabled(false);
+            	else
+            		((JCheckBoxMenuItem)box.getMenu(0).getItem(3)).setEnabled(true);
+            	
+            	//Verify checked options
+            	for (int i = 0; i < box.getMenu(0).getItemCount(); i++) {
+            		JMenuItem j = box.getMenu(0).getItem(i);
+            		JCheckBoxMenuItem ci = (JCheckBoxMenuItem) j;
+            		res *= 2;
+            		if (ci.isEnabled() && ci.getState())
+            			res++;
+            	}
+            	
+            	panel.setCurrentView(res);
             }
-        }
+        //}
     }
-
 } // Class
