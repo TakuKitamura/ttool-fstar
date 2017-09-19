ac06938251db12f92a987c74b062a5ea707a74ea
Fabien Tessier <fabien.tessier@telecom-paristech.fr>
2017-09-19 13:13:26 +0200

    Add javadoc and comments for new class JDialogBase

--- src/main/java/ui/window/JDialogBase.java
+++ src/main/java/ui/window/JDialogBase.java
@@ -16,6 +16,14 @@ import javax.swing.KeyStroke;
 
 import ui.util.IconManager;
 
+/**
+ * Class JDialogBase
+ * Superclass for dialog inheritance 
+ * Creation: 19/09/2017
+ * @version 1.0 19/09/2017
+ * @author Fabien TESSIER
+*/
+
 public class JDialogBase extends JDialog {
 	
 	protected JDialogBase(Frame _frame, String _title, boolean b) {
@@ -31,6 +39,8 @@ public class JDialogBase extends JDialog {
 	
 	protected JDialogBase(Frame _frame, String _title, Dialog.ModalityType m) {
 		super(_frame, _title, m);
+		
+		//Add closeButton's behaviour on Escape key
 		getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("ESCAPE"), "close");
         getRootPane().getActionMap().put("close", new AbstractAction() {
         	@Override
@@ -42,13 +52,19 @@ public class JDialogBase extends JDialog {
 	
 	protected void initButtons(JButton closeButton, JButton cancelButton, GridBagConstraints c0, Container c,
 			                  ActionListener al) {
+		//Close Button
         closeButton = new JButton("Save and Close", IconManager.imgic25);
         closeButton.addActionListener(al);
         c.add(closeButton, c0);
+        
         c0.gridwidth = GridBagConstraints.REMAINDER; //end row
+        
+        //Cancel Button
         cancelButton = new JButton("Cancel", IconManager.imgic27);
         cancelButton.addActionListener(al);
         c.add(cancelButton, c0);
+        
+        //Add closeButton's behaviour on Enter key
         this.getRootPane().setDefaultButton(closeButton);
 ;
 	}
