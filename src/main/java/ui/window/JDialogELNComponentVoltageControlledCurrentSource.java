71079caff6cb55dffb4a8f3a123925af42869616
Etudiant Master SESI <irina.lee@etu.upmc.fr>
2018-06-19 10:59:13 +0200

    modified jdialog for eln

--- src/main/java/ui/window/JDialogELNComponentVoltageControlledCurrentSource.java
+++ src/main/java/ui/window/JDialogELNComponentVoltageControlledCurrentSource.java
@@ -43,6 +43,7 @@ import ui.util.IconManager;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -200,7 +201,7 @@ public class JDialogELNComponentVoltageControlledCurrentSource extends JDialog i
 		attributesMainPanel.add(box); // add grid to grid
 
 		// Down Side
-		JPanel downPanel = new JPanel(new GridLayout(1, 2));
+		JPanel downPanel = new JPanel(new FlowLayout());
 
 		JButton saveCloseButton = new JButton("Save and close");
 		saveCloseButton.setIcon(IconManager.imgic25);
