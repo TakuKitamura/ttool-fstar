a9df5958908a1fd55668ae9952592a49d63e5145
Ludovic Apvrille <apvrille@cadillac.eurecom.fr>
2018-10-25 03:01:34 +0200

    update on build version: build.txt

--- src/main/java/ui/util/DefaultText.java
+++ src/main/java/ui/util/DefaultText.java
@@ -50,8 +50,8 @@ package ui.util;
  */
 public class DefaultText {
 
-    public static String BUILD = "12819";
-    public static String DATE = "2018/10/24 03:01:35 CET";
+    public static String BUILD = "12820";
+    public static String DATE = "2018/10/25 03:01:34 CET";
 
     public static StringBuffer sbAbout = makeAbout();
 
