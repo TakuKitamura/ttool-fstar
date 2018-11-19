8790a49a6a7c39dbe73945359c40bf7b196c903c
Arthur VUAGNIAUX <arthur.vuagniaux@telecom-paristech.fr>
2018-11-19 15:22:08 +0100

    Issue #71: Added files

---
+++ ttool/src/test/java/ui/bot/ModelCreationTests.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright or (C) or Copr. GET / ENST, Telecom-Paris, Arthur VUAGNIAUX
+ */
+
+package ui.bot;
+
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+
+/*
+ * Class ModelCreationTests
+ * Creation: 19/11/2018
+ * @version 1.0 19/11/2018
+ * @author Arthur VUAGNIAUX
+*/
+
+public class ModelCreationTests extends AssertJSwingJUnitTestCase {
+	private FrameFixture window;
+	
+	@Override
+	protected void onSetUp() {
+		Main frame = GuiActionRunner.execute(()-> new Main(false, false, false, false, false, false, false, false, false, false, false, false, false));
+		window = new FrameFixture(robot(), frame.getFrame());
+		window.show();
+	}
+	
+	@Override
+	protected void onTearDown() {
+		super.onTearDown();
+		window.cleanUp();
+	}
+}
