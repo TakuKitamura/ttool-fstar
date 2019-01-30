be481e005311714d2a08b6d7b934c1f6915cc0ee
dblouin <dominique.blouin@telecom-paristech.fr>
2019-01-30 13:56:24 +0100

    Initialize old zoom factor properly

--- src/main/java/ui/TGCScalableWithoutInternalComponent.java
+++ src/main/java/ui/TGCScalableWithoutInternalComponent.java
@@ -53,6 +53,9 @@ public abstract class TGCScalableWithoutInternalComponent extends TGCWithoutInte
 
     public TGCScalableWithoutInternalComponent(int _x, int _y, int _minX, int _maxX, int _minY, int _maxY, boolean _pos, TGComponent _father, TDiagramPanel _tdp) {
         super(_x, _y, _minX, _maxX, _minY, _maxY, _pos, _father, _tdp);
+        
+        // Isssue #31
+        oldScaleFactor = 1.0;
     }
 
     @Override
