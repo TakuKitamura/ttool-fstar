f2e636ef05976ac8ce826d98f344c6f5721a3560
errea <javier.errea-moreno@eurecom.fr>
2019-02-07 16:43:47 +0100

    Arduino and MBED merged

--- modeling/Blinking.ttool/AVATAR_executablecode/generated_src/main.h
+++
@@ -1,8 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-/* Main mutex */
-extern pthread_mutex_t __mainMutex;
-
-/* Synchronous channels */
-/* Asynchronous channels */
-#endif
--- modeling/BlinkingLed.ttool/AVATAR_executablecode/generated_src/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-/* Main mutex */
-extern pthread_mutex_t __mainMutex;
-
-/* Synchronous channels */
-extern syncchannel __MainBlock_RandVal__SecondaryBlock_RanVal;
-/* Asynchronous channels */
-#endif
