a065773d2b86ef8c7e0e7a76a35e8a08a585e14c
Ludovic Apvrille <ludovic.apvrille@telecom-paristech.fr>
2019-04-09 14:05:57 +0200

    Revert "Merge branch 'minh-help' into 'master'"
    This reverts merge request !148

--- src/main/java/ui/window/JDialogCPUNode.java
+++ src/main/java/ui/window/JDialogCPUNode.java
@@ -103,7 +103,7 @@ public class JDialogCPUNode extends JDialogBase implements ActionListener  {
     //issue 183
     List<JButton>   buttons = new ArrayList<>();
     List<HelpEntry> helpEntries;
-    TGComponentHelp cpuHelp;
+    JFrameHWNodeHelp cpuHelp;
 
     /* Creates new form  */
     public JDialogCPUNode(MainGUI _mgui, Frame _frame, String _title, TMLArchiCPUNode _node, ArchUnitMEC _MECType,
@@ -132,20 +132,32 @@ public class JDialogCPUNode extends JDialogBase implements ActionListener  {
 
     //issue 183
     private void buttonClick(JButton but, HelpEntry he) {
-        but.setSelected(true);
+        //setModalityType(ModalityType.MODELESS);
         but.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 if(cpuHelp == null ) {
-                    cpuHelp = new TGComponentHelp(mgui, he);
-                    cpuHelp.setLocationHelpWindow(but);
+                    cpuHelp = new JFrameHWNodeHelp(mgui,"Help",he);
+                    cpuHelp.setLocationRelativeTo(but);
                 }else{
                     if(!cpuHelp.isVisible()) {
-                        cpuHelp = new TGComponentHelp(mgui, he);
-                        cpuHelp.setLocationHelpWindow(but);
+                        cpuHelp = new JFrameHWNodeHelp(mgui,"Help",he);
+                        cpuHelp.setLocationRelativeTo(but);
                     }else{
                         cpuHelp.setVisible(false);
                     }
+                }
+
+                if(cpuHelp != null) {
+                    cpuHelp.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("ESCAPE"), "close");
+                    cpuHelp.getRootPane().getActionMap().put("close", new AbstractAction() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            if(!cpuHelp.isVisible())
+                                dispose();
+                            cpuHelp.setVisible(false);
+                        }
+                    });
                 }
             }
         });
@@ -581,18 +593,12 @@ public class JDialogCPUNode extends JDialogBase implements ActionListener  {
         regularClose = true;
         MECType = ArchUnitMEC.Types.get( MECTypeCB.getSelectedIndex() );
         dispose();
-        if ((cpuHelp != null) && cpuHelp.isVisible()) {
-            cpuHelp.setVisible(false);
-        }
     }
 
     public void cancelDialog() {
 
         //TraceManager.addDev("Cancel dialog");
         dispose();
-        if ((cpuHelp != null) && cpuHelp.isVisible()) {
-            cpuHelp.setVisible(false);
-        }
     }
 
     public boolean isRegularClose() {
