49f7c2005e5f0123cff92ccdd0402862eda5f218
Moemoea Fierin <moemoea.fierin@epita.fr>
2019-09-26 13:10:06 +0200

    [ZOOM] Issue #31: Logo will now be zoomed in Req. Diagrams

--- src/main/java/ui/avatarrd/AvatarRDElementReference.java
+++ src/main/java/ui/avatarrd/AvatarRDElementReference.java
@@ -170,7 +170,7 @@ public class AvatarRDElementReference extends TGCScalableWithInternalComponent i
         g.fillRect(x+1, y+1+lineHeight, width-1, height-1-lineHeight);
         ColorManager.setColor(g, getState(), 0);
         if ((lineHeight > 23) && (width > 23)){
-            g.drawImage(IconManager.img5100, x + width - iconSize + 1, y + 3, Color.yellow, null);
+            g.drawImage(scale(IconManager.img5100), x + width - scale(iconSize + 1), y + scale(3), Color.yellow, null);
         }
 
         if (displayText) {
