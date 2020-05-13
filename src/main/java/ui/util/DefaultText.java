49ee410a273553c80c072bfcfbe8113e934749fa
Ludovic Apvrille <ludovic.apvrille@telecom-paristech.fr>
2020-05-13 15:34:53 +0200

    Revert "Translated String strip to trim for java 8 compatibility"
    This reverts commit 6a134982412d41c93128dfa551496d2208a6adaa.

--- src/main/java/ui/util/DefaultText.java
+++ src/main/java/ui/util/DefaultText.java
@@ -50,8 +50,8 @@ package ui.util;
  */
 public class DefaultText {
 
-    public static String BUILD = "13398";
-    public static String DATE = "2020/05/13 11:41:10 CET";
+    public static String BUILD = "13396";
+    public static String DATE = "2020/05/13 03:11:56 CET";
 
     public static StringBuffer sbAbout = makeAbout();
 
