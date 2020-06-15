59fe9c2ceff0b7c9b84938bef023d98d19403d12
Ludovic Apvrille <ludovic.apvrille@telecom-paris.fr>
2020-06-15 03:13:34 +0200

    update on build version: build.txt

--- src/main/java/ui/util/DefaultText.java
+++ src/main/java/ui/util/DefaultText.java
@@ -50,8 +50,8 @@ package ui.util;
  */
 public class DefaultText {
 
-    public static String BUILD = "13436";
-    public static String DATE = "2020/06/14 03:13:30 CET";
+    public static String BUILD = "13437";
+    public static String DATE = "2020/06/15 03:13:34 CET";
 
     public static StringBuffer sbAbout = makeAbout();
 
