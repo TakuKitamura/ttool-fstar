dd69027d5988be649caefb6f7d3d2e0361478fa2
tempiaa <tempiaa@eurecom.fr>
2020-07-01 15:14:49 +0200

    Cleaning imports

--- src/main/java/avatartranslator/modelchecker/CounterexampleTrace.java
+++ src/main/java/avatartranslator/modelchecker/CounterexampleTrace.java
@@ -7,9 +7,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
-import com.sun.net.httpserver.Authenticator.Result;
-
 import avatartranslator.AvatarBlock;
 import avatartranslator.AvatarSpecification;
 
