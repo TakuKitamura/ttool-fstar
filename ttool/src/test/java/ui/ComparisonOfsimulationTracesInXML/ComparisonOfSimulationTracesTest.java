db5d28012c13c66b736e36867ba2b50452a49af5
maysam zoor <maysam_zoor@hotmail.com>
2021-02-12 12:27:53 +0100

    update package

--- ttool/src/test/java/ui/ComparisonOfsimulationTracesInXML/ComparisonOfSimulationTracesTest.java
+++ ttool/src/test/java/ui/ComparisonOfsimulationTracesInXML/ComparisonOfSimulationTracesTest.java
@@ -10,7 +10,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.xml.sax.SAXException;
 
-import simulationtraceanalysis.CompareSimulationTrace;
+import tmltranslator.simulationtraceanalysis.CompareSimulationTrace;
 import ui.AbstractUITest;
 import ui.SimulationTrace;
 import ui.interactivesimulation.JFrameShowLatencyDetails;
