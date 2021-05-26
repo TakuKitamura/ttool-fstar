32dfa19edf40abc4a49a77cbab937e710297d73b
TakuKitamura <takukitamura.io@gmail.com>
2021-05-26 09:51:03 +0900

    create generateFstar.templ

--- playground/generateFstar/GenerateFstar.java
+++ playground/generateFstar/GenerateFstar.java
@@ -1,6 +1,36 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class GenerateFstar {
+    public static String fstarCode;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        try {
+            fstarCode = readAll("./fstar.templ");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        fstarCode = fstarCode.replaceAll("@module_name@", "Add");
+        fstarCode = fstarCode.replaceAll("@ret_integer_type@", "I32");
+        fstarCode = fstarCode.replaceAll("@ret_integer_type_suffix@", "l");
+        fstarCode = fstarCode.replaceAll("@arg_constraint@",
+                "let xConstraint x = I32.gte x 1l && I32.lte x 100l\nlet yConstraint y = I32.gte y 1l && I32.lte y 100l");
+        fstarCode = fstarCode.replaceAll("@ret_constraint@",
+                "let retConstraint ret = I32.gte ret 2l && I32.lte ret 200l");
+        fstarCode = fstarCode.replaceAll("@interface_func_name@", "add");
+        fstarCode = fstarCode.replaceAll("@interface_func_body_arg@", "x: I32.t ->\n  y: I32.t ->");
+        fstarCode = fstarCode.replaceAll("@interface_func_body_arg_require@", "xConstraint x && yConstraint y");
+        fstarCode = fstarCode.replaceAll("@args@", "x y");
+        fstarCode = fstarCode.replaceAll("@unimplemented_func_ret_value@", "2");
+        fstarCode = fstarCode.replaceAll("@interface_func_arg@", "x: I32.t ->\n  y: I32.t ->");
+        fstarCode = fstarCode.replaceAll("@valid_func_body_call_condition@", "(xConstraint x) && (yConstraint y)");
+        System.out.println(fstarCode);
+    }
+
+    public static String readAll(String path) throws IOException {
+        return Files.lines(Paths.get(path)).reduce("",
+                (prev, line) -> prev + line + System.getProperty("line.separator"));
     }
 }
--- playground/generateFstar/fstarSample/fstar-add/add.fst
+++ playground/generateFstar/fstarSample/fstar-add/add.fst
@@ -5,14 +5,7 @@ open FStar.HyperStack.ST
 open LowStar.Printf
 open C.String
 
-module B = LowStar.Buffer
-module U8 = FStar.UInt8
-module U32 = FStar.UInt32
 module I32 = FStar.Int32
-
-let xrange x = I32.gte x 1l && I32.lte x 100l
-let yrange y = I32.gte y 1l && I32.lte y 100l
-let retRange ret = I32.gte ret 2l && I32.lte ret 200l
 
 type struct_error = {
   code: I32.t;
@@ -41,16 +34,30 @@ let ng_ret: struct_ret = {
     error = ng_error;
 }
 
+let xrange x = I32.gte x 1l && I32.lte x 100l
+let yrange y = I32.gte y 1l && I32.lte y 100l
+let retRange ret = I32.gte ret 2l && I32.lte ret 200l
+
 val add_internal:
-  x: I32.t{xrange x} ->
-  y: I32.t{yrange y} ->
-  ret: I32.t{retRange ret}
+  x: I32.t ->
+  y: I32.t ->
+Stack (ret: I32.t) (requires fun h0 -> 
+    xrange x && yrange y
+  )
+  (ensures fun h0 ret h1 -> 
+    retRange ret
+  )
 let add_internal x y = I32.add x y
 
 val add: 
   x: I32.t ->
   y: I32.t ->
-  ret: struct_ret{(retRange ret.value && I32.v ret.error.code = 0) || (I32.v ret.error.code > 0)}
+  Stack (ret: struct_ret) (requires fun h0 -> 
+    true
+  )
+  (ensures fun h0 ret h1 -> 
+    (retRange ret.value && I32.v ret.error.code = 0) || (I32.v ret.error.code > 0)
+  )
 let add x y = 
   if (xrange x) && (yrange y) then
     {
