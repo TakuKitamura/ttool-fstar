a0cbaa29d523da683b7eeab82a660a7f88ec3da1
TakuKitamura <takukitamura.io@gmail.com>
2021-07-28 11:20:19 +0900

    remove no need files

--- playground/generateFstar/fstarSample/fstar-add/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "Add.h"
-
-int main()
-{
-    printf("%d\n", add(100, 100));
-    return 0;
-}
--- src/main/java/fstar/transpiler/MethodDeclaration.java
+++
@@ -1,9 +0,0 @@
-package fstar.transpiler;
-
-import java.util.LinkedHashMap;
-
-public class MethodDeclaration {
-    String funcName;
-    String returnType;
-    LinkedHashMap<String, String> args;
-}
