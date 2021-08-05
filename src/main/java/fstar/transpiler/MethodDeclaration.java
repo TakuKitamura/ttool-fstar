987b908d665a1d3ce9e8cefd02412859ec585a7d
TakuKitamura <takukitamura.io@gmail.com>
2021-08-05 09:53:26 +0900

    add fstar transpiler

---
+++ src/main/java/fstar/transpiler/MethodDeclaration.java
@@ -0,0 +1,9 @@
+package fstar.transpiler;
+
+import java.util.LinkedHashMap;
+
+public class MethodDeclaration {
+    String funcName;
+    String returnType;
+    LinkedHashMap<String, String> args;
+}
